2c0b3d1ad22002c99ffc099fae655653f559a622
sagieske <sharongieske@gmail.com>
2014-11-12 16:58:56 +0100

    flipping policy grid function

--- one/helpers.py
+++ one/helpers.py
@@ -1,5 +1,7 @@
 import numpy as np
 import math
+from operator import add,mul, sub
+import sys
 
 def xy_distance(predator_location, prey_location, grid_size, toroidal=True):
 	""" Calculate xy distance using a toroidal grid"""
@@ -16,7 +18,13 @@ def get_rotation(predator_location, prey_location):
 	""" Returns unit vector of distance to predator location using prey_location as center """
 	x_rotation = predator_location[0] - prey_location[0] 
 	y_rotation = predator_location[1] - prey_location[1] 
-	return [cmp(x_rotation,0), cmp(y_rotation,0)]
+	rot_x = cmp(x_rotation,0)
+	rot_y = cmp(y_rotation,0)
+	if rot_x == 0:
+		rot_x = 1
+	if rot_y == 0:
+		rot_y = 1
+	return [rot_x, rot_y]
 
 def euclidian(first_location, second_location):
 	"""  Calculates euclidian distance"""
@@ -79,3 +87,59 @@ def pretty_print_latex(matrix, label, indices=True):
 	print "\\hline"
 	print "\\end{tabular}"
 
+
+def full_policy_grid_from_encoding(goal_state, encoded_grid, gridsize=[11,11]):
+	""" Create full grid from partial grid created by encoded state space for policy grid"""
+	full_grid = np.zeros((gridsize[0],gridsize[1]))
+
+	old_policy = {"North":0, "West":0, "East":0, "South":0, "Wait":0}
+	full_grid = [[old_policy for k in range(0, gridsize[1])] for l in range(0,  gridsize[0])]
+
+	# Fill in full grid
+	for x in range(0,gridsize[0]):
+		for y in range(0,gridsize[1]):
+			# Get relative distance
+			distance = xy_distance([x,y], goal_state, gridsize)
+			# Get true distance
+			true_distance = map(sub, [x,y], goal_state)
+
+			# Get rotation
+			rotation =  get_rotation([x,y], goal_state)
+
+			# Wanna flip rotation if relative distance is smaller than normal distance (due to toroidality)
+			if distance[0] < abs(true_distance[0]):
+				rotation[0] = -(rotation[0])
+
+			if distance[1] < abs(true_distance[1]):
+				rotation[1] = -(rotation[1])
+
+			# Get policy of state
+			policy_state = encoded_grid[distance[0]][distance[1]]
+			new_policy = flip_policy(policy_state,rotation)
+
+			# Flip all policies accordingly
+			# Get value from encoded grid using relative distance
+			full_grid[x][y] = new_policy	
+	return full_grid
+
+def flip_policy(policy, rotation):
+	"""Rotates actions according to 'angle' from prey location"""
+	actions = {'North': [-1,0], 'East': [0,1], 'South': [1,0],'West': [0,-1], 'Wait':[0,0]}
+	action_name_list, movement = zip(*actions.items())
+	# Create copy for new policy
+	new_policy = policy.copy()
+	# rotate actions
+	for index in range(0,len(movement)):
+		old_action = action_name_list[index]
+		old_movement = movement[index]
+		new_movement = map(mul, rotation, old_movement)
+		index_new_movement = movement.index(new_movement)
+		new_action = action_name_list[index_new_movement]
+
+		# Flip values
+		if old_action != new_action and policy[old_action] == 1:
+			new_policy[old_action] = 0
+			new_policy[new_action] = 1
+
+	return new_policy
+	
